580ea92ee4e3a17a687b25c2afb1127377f30933
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-03 09:49:18 -0300

    feat: creating new django apps.

---
+++ src/application/django/apps/active/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ActiveConfig(AppConfig):
+    name = 'src.application.django.apps.active'
---
+++ src/application/django/apps/management/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ManagementConfig(AppConfig):
+    name = 'src.application.django.apps.management'
