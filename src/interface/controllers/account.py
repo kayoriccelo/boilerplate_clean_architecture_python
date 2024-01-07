c0b3a174d554543753837796e444ffadc6e54127
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-07 13:46:37 -0300

    feat: created account controller interface.

---
+++ src/interface/controllers/account.py
@@ -0,0 +1,36 @@
+from http import HTTPStatus
+from typing import Tuple
+
+from src.use_cases.account import AccountUseCase
+
+
+class AccountController:
+    def __init__(self, use_case: AccountUseCase):
+        self.use_case = use_case
+
+    def get(self, pk: str) -> Tuple[dict, int]:
+        pk = pk.upper()
+
+        try:
+            account = self.use_case.get(pk)
+
+        except Exception as err:
+            return {'error': err.message}, HTTPStatus.NOT_FOUND.value
+
+        
+        return account, HTTPStatus.OK.value
+
+    def list(self) -> Tuple[list, int]:
+        accounts = self.use_case.get_availables()
+
+        return accounts, HTTPStatus.OK.value
+
+    def create(self, data: dict) -> int:
+        self.use_case.create()
+        
+        return HTTPStatus.OK.value
+
+    def update(self, data: dict) -> int:
+        self.use_case.update(data)
+
+        return HTTPStatus.OK.value
