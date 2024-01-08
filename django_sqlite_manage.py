5299f40ac02cbee4691c137f241c40f438279204
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-08 09:01:53 -0300

    refactor: django settings module changed.

--- django_sqlite_manage.py
+++ django_sqlite_manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.application.django.sqlite.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.infrastructure.settings.django.sqlite.settings')
 
     try:
         from django.core.management import execute_from_command_line
