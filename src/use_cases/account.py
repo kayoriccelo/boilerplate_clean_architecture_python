3385fa52fc4d73762cef4d3cf6a28f6e6793fb38
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-08 00:03:05 -0300

    refactor: changed the type of the pk parameter of the account use case.

--- src/use_cases/account.py
+++ src/use_cases/account.py
@@ -1,4 +1,3 @@
-
 from typing import List
 
 from src.domain.entities.account import Account
@@ -8,7 +7,7 @@ class AccountUseCase:
     def __init__(self, account_repo: object):
         self.account_repo = account_repo
 
-    def get(self, pk: str) -> Account:
+    def get(self, pk: int) -> Account:
         return self.account_repo.get(pk)
 
     def get_availables(self) -> List[Account]:
@@ -19,12 +18,12 @@ class AccountUseCase:
 
         self.account_repo.create(account)
 
-    def update(self, pk: str, account: Account):
+    def update(self, pk: int, account: Account):
         # TODO - Kayo: add rules.
 
         self.account_repo.create(pk, account)
 
-    def delete(self, pk: str):
+    def delete(self, pk: int):
         # TODO - Kayo: add rules.
 
         self.account_repo.delete(pk)
