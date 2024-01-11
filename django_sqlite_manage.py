791a220e0ae448afbf5c631c9d63f665effaff04
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-11 12:12:00 -0300

    refactor: renamed route in django settings module.

--- django_sqlite_manage.py
+++ django_sqlite_manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.infrastructure.settings.django.sqlite.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.infrastructure.django.settings.sqlite.settings')
 
     try:
         from django.core.management import execute_from_command_line
