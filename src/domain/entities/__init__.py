e73597e25329f574cbe1b0d165b8018b312dbca6
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-11 11:59:25 -0300

    refactor: added default value in active entity field and added class type in fields.

---
+++ src/domain/entities/__init__.py
@@ -0,0 +1,3 @@
+from src.domain.entities.account import Account
+from src.domain.entities.broker import Broker
+from src.domain.entities.active import Active
--- src/domain/entities/active.py
+++ src/domain/entities/active.py
@@ -1,8 +1,13 @@
 from dataclasses import dataclass
+from datetime import datetime
+
+from src.domain.entities import Account, Broker
 
 
 @dataclass
 class Active:
+    id: int = None
+    created: datetime = datetime.now()
     description: str = None
-    account: int = None
-    broker: int = None
+    account: Account = None
+    broker: Broker = None
