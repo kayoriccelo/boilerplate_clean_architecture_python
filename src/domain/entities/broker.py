10cdf52a1a08d3398c2de8bc1a1b4ee07c7a35dc
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-11 12:00:01 -0300

    refactor: added default value in broker entity field and added class type in fields.

--- src/domain/entities/broker.py
+++ src/domain/entities/broker.py
@@ -1,7 +1,12 @@
 from dataclasses import dataclass
+from datetime import datetime
+
+from src.domain.entities import Account
 
 
 @dataclass
 class Broker:
+    id: int = None
+    created: datetime = datetime.now()
     description: str = None
-    account: int = None
+    account: Account = None
