119bfd0d6e57909fd5240669c85bdec53b4f1102
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-11 12:31:58 -0300

    feat: presenter created for account interface.

---
+++ src/interface/presenters/__init__.py
@@ -0,0 +1,3 @@
+from src.interface.presenters.account import AccountPresenter
+from src.interface.presenters.broker import BrokerPresenter
+from src.interface.presenters.active import ActivePresenter
