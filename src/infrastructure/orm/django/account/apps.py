764912fc512fa265de6762d72f814bea95eac068
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-11 12:02:16 -0300

    refactor: removed infrastructure ORM.

--- src/infrastructure/orm/django/account/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AccountConfig(AppConfig):
-    name = 'src.infrastructure.orm.django.account'
--- src/infrastructure/orm/django/cycle/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CycleConfig(AppConfig):
-    name = 'src.infrastructure.orm.django.cycle'
