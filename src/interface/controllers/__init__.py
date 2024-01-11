1205d86d8f25240f1dcde34268a0d0f0dbd646ee
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-11 12:31:41 -0300

    feat: controller created for account interface.

---
+++ src/interface/controllers/__init__.py
@@ -0,0 +1,3 @@
+from src.interface.controllers.account import AccountController
+from src.interface.controllers.broker import BrokerController
+from src.interface.controllers.active import ActiveController
--- src/interface/controllers/account.py
+++ src/interface/controllers/account.py
@@ -1,39 +1,6 @@
-from http import HTTPStatus
-from typing import Tuple
-
-from src.use_cases.account import AccountUseCase
+from src.interface._common.controller import BaseController
+from src.use_cases.account.business import AccountBusiness
 
 
-class AccountController:
-    def __init__(self, repository: object):
-        self.use_case = AccountUseCase(repository)
-
-    def get(self, pk: int) -> Tuple[dict, int]:
-        try:
-            account = self.use_case.get(pk)
-
-        except Exception as err:
-            return {'error': err.message}, HTTPStatus.NOT_FOUND.value
-
-        
-        return account, HTTPStatus.OK.value
-
-    def list(self) -> Tuple[list, int]:
-        accounts = self.use_case.get_availables()
-
-        return accounts, HTTPStatus.OK.value
-
-    def create(self, data: dict) -> int:
-        self.use_case.create(data)
-        
-        return HTTPStatus.OK.value
-
-    def update(self, data: dict) -> int:
-        self.use_case.update(data)
-
-        return HTTPStatus.OK.value
-
-    def delete(self, pk: int) -> int:
-        self.use_case.delete(pk)
-
-        return HTTPStatus.OK.value
+class AccountController(BaseController):
+    business_class = AccountBusiness
