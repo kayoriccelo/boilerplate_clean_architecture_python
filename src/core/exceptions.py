fee7ee172f42b9619bfce14e9401b38c04a62c03
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-14 12:45:07 -0300

    feat: created standard exceptions.

---
+++ src/core/exceptions.py
@@ -0,0 +1,16 @@
+
+class ValueObjectExcepiton(Exception): pass
+
+class EntityException(Exception): pass
+
+class UseCaseException(Exception): pass
+
+class ControllerException(Exception): pass
+
+class ValidatorException(Exception): pass
+
+class PresenterException(Exception): pass
+
+class RepositoryException(Exception): pass
+
+class ViewException(Exception): pass
