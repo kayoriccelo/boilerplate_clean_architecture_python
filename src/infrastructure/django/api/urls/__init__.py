d2d15f779ed2401abe905eef0bdf04d359318ee6
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-14 12:46:33 -0300

    refactor: removed django api from the infrasctructure.

--- src/infrastructure/django/api/urls/__init__.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-
-
-urlpatterns = [
-    path("accounts/", include("src.infrastructure.django.api.urls.account")),
-    path("brokers/", include("src.infrastructure.django.api.urls.broker")),
-    path("active/", include("src.infrastructure.django.api.urls.active")),
-]
