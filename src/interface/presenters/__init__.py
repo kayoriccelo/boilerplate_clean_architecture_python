8f9cd7c51f158da5fe202e673826fad0d5ad24bb
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-15 00:24:03 -0300

    refactor: removed unsed presenters.

--- src/interface/presenters/__init__.py
+++ src/interface/presenters/__init__.py
@@ -1,3 +1,1 @@
 from src.interface.presenters.account import AccountPresenter
-from src.interface.presenters.broker import BrokerPresenter
-from src.interface.presenters.active import ActivePresenter
