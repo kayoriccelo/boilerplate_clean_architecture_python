67115377357b4e10d152b87024f75e185c6b7d97
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-14 23:44:29 -0300

    refactor: removed account file in controllers.

--- src/interface/controllers/account.py
+++
@@ -1,6 +0,0 @@
-from src.interface._common.controller import BaseController
-from src.use_cases.account.business import AccountBusiness
-
-
-class AccountController(BaseController):
-    business_class = AccountBusiness
