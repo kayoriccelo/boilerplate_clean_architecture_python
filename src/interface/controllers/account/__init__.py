39662ff9cabc20754c34d6a0d4a89c4c9414ad0b
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-14 23:55:49 -0300

    feat: account controller created.

---
+++ src/interface/controllers/account/__init__.py
@@ -0,0 +1,3 @@
+from src.interface.controllers.account.controller import AccountController
+# from src.interface.controllers.broker.controller import BrokerController
+# from src.interface.controllers.active.controller import ActiveController
