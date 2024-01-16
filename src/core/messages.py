c581823feb491e60f581d3b99e2ac62e367122f9
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-16 08:08:52 -0300

    feat: create new system inconsistency message.

--- src/core/messages.py
+++ src/core/messages.py
@@ -1,3 +1,8 @@
+INCONSISTENCY_MESSAGE_FOUND_SYSTEM = (
+    "The system encountered an inconsistency when trying to perform the operation. "
+    "Please wait a moment while our technical team checks."
+)
+
 INCONSISTENCY_MESSAGE_FOUND = (
     "The system encountered an inconsistency when trying %s. "
     "Please wait a moment while our technical team checks."
