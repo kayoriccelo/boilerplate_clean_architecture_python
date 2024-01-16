81e0c445e6e478ec4cd96241508226e8250fbcb4
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-16 08:12:02 -0300

    refactor: Improved messaging exception.

--- src/infrastructure/_common/repositories.py
+++ src/infrastructure/_common/repositories.py
@@ -13,7 +13,9 @@ class BaseModelRepository:
         instance = self.class_model.objects.filter(pk=pk).values().first()
         
         if not instance:
-            raise RepositoryException(Exception(f'{pk} pk does not exist'), f'record not found.')
+            err = Exception(f'pk {pk} does not exist')
+
+            raise RepositoryException(err, 'record not found')
         
         return self.class_entity(**instance)
 
@@ -27,7 +29,7 @@ class BaseModelRepository:
             self.class_model.objects.create(**instance_dict)
 
         except Exception as err:
-            raise RepositoryException(err, 'error when trying to create model instance.')
+            raise RepositoryException(err, 'error when trying to create model instance')
 
     def update(self, pk: int, instance: object):
         try:
@@ -36,11 +38,11 @@ class BaseModelRepository:
             self.class_model.objects.filter(pk=pk).update(**instance_dict)
 
         except Exception as err:
-            raise RepositoryException(err, 'error when trying to update model instance.')
+            raise RepositoryException(err, 'error when trying to update model instance')
         
     def delete(self, pk: int):
         try:
             self.class_model.objects.filter(pk=pk).delete()
 
         except Exception as err:
-            raise RepositoryException(err, 'error when trying to delete model instance.')
+            raise RepositoryException(err, 'error when trying to delete model instance')
