2490c244be3b9312c47042114327a0b922f713a4
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-17 12:23:40 -0300

    refactor: created a new method to check if the account exists in the account repository.

--- src/infrastructure/orm/django/apps/account/repositories.py
+++ src/infrastructure/orm/django/apps/account/repositories.py
@@ -6,3 +6,10 @@ from src.infrastructure.orm.django.apps.account.models import AccountModel
 class AccountModelRepository(BaseModelRepository):
     class_model = AccountModel
     class_entity = Account
+
+    def account_exists(self, account: Account) -> bool:
+        accounts_model = self.class_model.objects.filter(
+            number_identity=account.number_identity
+        )
+
+        return accounts_model.exists()
