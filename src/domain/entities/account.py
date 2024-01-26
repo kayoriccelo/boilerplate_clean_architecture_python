248b37add7894cb5a7c08389762121070a0134e1
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-26 15:11:47 -0300

    refactor: adapted account entity with value object.

--- src/domain/entities/account.py
+++ src/domain/entities/account.py
@@ -1,14 +1,27 @@
-from dataclasses import dataclass
+
 from datetime import datetime
+from enum import Enum
+
+from src.domain._common.entity import BaseEntity
+from src.domain.values_object import UUIDValue, DateTimeValue, CharValue, ChoiceValue
 
 
-@dataclass
-class Account:
-    id: int = None
-    created: datetime = datetime.now()
-    first_name: str = None
-    last_name: str = None
-    number_identity: str = None
-    date_birth: datetime = None
-    gender: int = None
-    status: int = None
+class StatusAccount(Enum):
+    ACTIVE = 1
+    INACTIVE = 2
+
+
+class GenderAccount(Enum):
+    MALE = 1
+    FEMALE = 2
+
+
+class Account(BaseEntity):
+    id = UUIDValue()
+    created = DateTimeValue(auto_add=True, default=datetime.now())
+    first_name = CharValue(max_length=200)
+    last_name = CharValue(max_length=200)
+    number_identity = CharValue(max_length=20)
+    date_birth = DateTimeValue()
+    gender = ChoiceValue(enum=GenderAccount)
+    status = ChoiceValue(enum=StatusAccount, default=StatusAccount.ACTIVE.value)
