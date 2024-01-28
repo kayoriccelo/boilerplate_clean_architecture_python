97ac5a1b9d7b905b02a0bcd2c31197985ab223d0
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-28 13:07:40 -0300

    feat: created a common controller.

---
+++ tests/_common/controller.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+class BaseControllerTest:
+    controller_class = None
+
+    @pytest.fixture(scope="class", autouse=True)
+    def account_controller(self, account_repository):
+        return self.controller_class(account_repository)
