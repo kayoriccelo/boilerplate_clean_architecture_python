edbdfeca381f5244f03b6676e066196aebdab346
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-28 13:21:41 -0300

    refactor: improvement in common use case.

--- tests/_common/use_case.py
+++ tests/_common/use_case.py
@@ -1,47 +1,18 @@
 import pytest
 
 
-@pytest.mark.django_db
 class BaseUseCaseRuleTest:
-    entity_class = None
-    repository_class = None
     rules_class = None
 
-    @property
-    def data(self):
-        raise NotImplementedError('Implementation of the required method.')
-
-    @property
-    def entity(self):
-        return self.entity_class(**self.data)
-
-    @property
-    def repository(self):
-        return self.repository_class()
-    
     @property
     def rules(self):
         return self.rules_class()
 
 
-@pytest.mark.django_db
 class BaseUseCaseBusinessTest:
-    entity_class = None
-    repository_class = None
     business_class = None
-
-    @property
-    def data(self):
-        raise NotImplementedError('Implementation of the required method.')
-
-    @property
-    def entity(self):
-        return self.entity_class(**self.data)
-
-    @property
-    def repository(self):
-        return self.repository_class()
+    _business = None
     
-    @property
-    def business(self):
-        return self.business_class(self.repository)
+    @pytest.fixture(scope="class", autouse=True)
+    def account_business(self, account_repository):
+        return self.business_class(repository=account_repository)
