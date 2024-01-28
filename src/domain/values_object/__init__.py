68d949a25bc70f06bf2e8d7f55fbc998919bb969
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-28 11:54:10 -0300

    refactor: improvement in the date and datetime value object.

--- src/domain/values_object/__init__.py
+++ src/domain/values_object/__init__.py
@@ -1,4 +1,5 @@
 from src.domain.values_object.uuid import UUIDValue
+from src.domain.values_object.datetime import DateValue
 from src.domain.values_object.datetime import DateTimeValue
 from src.domain.values_object.char import CharValue
 from src.domain.values_object.choice import ChoiceValue
--- src/domain/values_object/datetime.py
+++ src/domain/values_object/datetime.py
@@ -2,10 +2,11 @@
 from datetime import datetime
 
 from src.core.exceptions import ValueObjectExcepiton
+
 from src.domain._common.values_object import ValueObject
 
 
-class DateTimeValue(ValueObject):
+class DateValue(ValueObject):
     auto_add = False
 
     def __init__(self, **kwargs) -> None:
@@ -19,18 +20,40 @@ class DateTimeValue(ValueObject):
                 value = getattr(instance, self.targe_name)
                 
                 if not value:
+                    setattr(instance, self.targe_name, datetime.now().date())
+
+        else:
+            if type(value) == str:
+                regex = datetime.strptime
+
+                try:
+                    if regex(value, "%Y-%m-%d"):
+                        value = datetime.strptime(value, "%Y-%m-%d").date()
+
+                except Exception:
+                    raise ValueObjectExcepiton('format invalid')
+                            
+        setattr(instance, self.targe_name, value)
+
+
+class DateTimeValue(DateValue):
+    def __set__(self, instance, value=None):
+        if self.auto_add:
+            if hasattr(instance, self.targe_name):
+                value = getattr(instance, self.targe_name)
+                
+                if not value:
                     setattr(instance, self.targe_name, datetime.now())
 
         else:
             if type(value) == str:
+                regex = datetime.strptime
+
                 try:
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M")
+                    if regex(value, "%Y-%m-%dT%H:%M"):
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M")
 
                 except Exception:
-                    try:
-                        value = datetime.strptime(value, "%Y-%m-%d")
-                        
-                    except Exception:
-                        raise ValueObjectExcepiton('format invalid')
-                
+                    raise ValueObjectExcepiton('format invalid')
+                            
         setattr(instance, self.targe_name, value)
