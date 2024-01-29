11da91f1e9b32a22f741770cffb9b53399410605
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-29 17:13:16 -0300

    refactor: relocated exceptions and created exception messages.

---
+++ src/core/exceptions/messages.py
@@ -0,0 +1,49 @@
+INCONSISTENCY_MESSAGE_FOUND_SYSTEM = (
+    "The system encountered an inconsistency when trying to perform the operation. "
+    "Please wait a moment while our technical team checks."
+)
+
+INCONSISTENCY_MESSAGE_FOUND = (
+    "The system encountered an inconsistency when trying %s. "
+    "Please wait a moment while our technical team checks."
+)
+
+FORMAT_INVALID_MESSAGE_EXCEPTION = "format is invalid."
+
+MAX_LENGHT_MESSAGE_EXCEPTION = "the maximum length is %s."
+
+PK_NOT_EXIST_MESSAGE_EXCEPTION = "pk %s does not exist."
+
+RECORD_NOT_FOUND_MESSAGE_EXCEPTION = "record not found"
+
+INFORMATION_REQUIRED_MESSAGE_EXCEPTION = "information required"
+
+CREATE_RECORD_MESSAGE_EXCEPTION = "create the record"
+
+CREATE_LISTING_RECORD_MESSAGE_EXCEPTION = "create listing records"
+
+ACCESS_PAGE_LISTING_MESSAGE_EXCEPTION = "access page %s of the listing"
+
+NOT_FOUND_IN_MESSAGE_EXCEPTION = "%s  not found in  %s"
+
+ALREADY_CREATED_MESSAGE_EXCEPTION = "%s already exists"
+
+OPERATION_WITHOUT_PERMISSION_MESSAGE_EXCEPTION = "Operation without permission"
+
+THERE_ARE_OUTSTANDING_CRITICISMS_MESSAGE_EXCEPTION = (
+    "There are outstanding criticisms"
+)
+
+ERROR_CREATE_MODEL_INSTANCE_MESSAGE_EXCEPTION = "error when trying to create model instance"
+
+ERROR_UPDATE_MODEL_INSTANCE_MESSAGE_EXCEPTION = "error when trying to update model instance"
+
+ERROR_DELETE_MODEL_INSTANCE_MESSAGE_EXCEPTION = "error when trying to delete model instance"
+
+ERROR_INSTANTIATE_REPOSITORY_MESSAGE_EXCEPTION = (
+    "error when trying to instantiate the repository"
+)
+
+ERROR_INSTANTIATE_CONTROLLER_MESSAGE_EXCEPTION = (
+    "error when trying to instantiate the controller."
+)
