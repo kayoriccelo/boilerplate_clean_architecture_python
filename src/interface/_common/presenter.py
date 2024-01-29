77ed1e291d0f10af7fb807d7e4d99f41161e63a0
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-29 07:53:40 -0300

    feat: created a base of presenters.

---
+++ src/interface/_common/presenter.py
@@ -0,0 +1,18 @@
+
+class BasePresenter:
+    serializer_class = None
+    data = {}
+
+    def __init__(self, serializer_class: any) -> None:
+        self.serializer_class = serializer_class
+
+    def parse(self, instance: object) -> dict:
+        result = self.serializer_class(instance)
+
+        if hasattr(result, 'data'):
+            self.data = result.data
+            
+        else:
+            self.data = result
+
+        return self.data
