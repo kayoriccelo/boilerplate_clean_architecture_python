4a2511604bcda12da9aaed151fc560b51d0db70d
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-01-29 12:50:40 -0300

    refactor: business adapted with the state.

--- src/use_cases/account/business.py
+++ src/use_cases/account/business.py
@@ -2,8 +2,10 @@
 from src.use_cases.account.rules import AccountRules
 from src.use_cases._common.business import BaseBusiness
 from src.domain.entities.account import Account
+from src.use_cases.account.state import AccountState
 
 
 class AccountBusiness(BaseBusiness):
     entity_class = Account
     rules_class = AccountRules
+    state_class = AccountState
