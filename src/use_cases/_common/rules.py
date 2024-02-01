8f8016da676de3e93e97dc5631937aa8912323b2
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-02-01 15:21:28 -0300

    docs: added documentation on common classes in use cases.

--- src/use_cases/_common/rules.py
+++ src/use_cases/_common/rules.py
@@ -6,7 +6,13 @@ from src.core.exceptions.types import SystemException, UseCaseRuleException
 
 
 class BaseRules:
-    """Base class for rules."""
+    """
+    Base class for rules.
+
+    This class provides methods for managing the state of the rule,
+    including exception management and callback execution.
+    In addition, it provides a method for getting a value in kwargs.
+    """
 
     _exception: bool = True
     _can: bool = True
@@ -15,6 +21,10 @@ class BaseRules:
     def _get_value_in_kwargs(self, name: str, required: bool = True) -> Any:
         """
         Get a value from the keyword arguments.
+
+        This method checks if the value is present in kwargs and, if necessary,
+        if it has been provided. If the value is not present and is required,
+        an error will be generated.
 
         Args:
             name (str): The name of the argument.
@@ -38,11 +48,17 @@ class BaseRules:
         """
         Execute an exception.
 
+        This method checks if exceptions are enabled and, if so,
+        executes an exception. Then it returns the rule state.
+
         Args:
             message (str): The message of the exception.
 
         Raises:
-            UseCaseRuleException: If the exception is enabled.
+            UseCaseRuleException: If exceptions are enabled.
+
+        Returns:
+            bool: The rule state.
         """
 
         if self._exception:
@@ -54,13 +70,17 @@ class BaseRules:
         """
         Execute a callback.
 
+        This method checks if the callback returned True and, if not,
+        calls the callback. Then it checks if the callback returned False and, if so,
+        generates an exception.
+
         Args:
             callback (Callable[[], bool]): The callback to execute.
 
         Raises:
-            SystemException: If the callback returns False and the can flag is False.
+            SystemException: If the callback returns False and the rule state is False.
         """
-
+        
         if not self._can:
             self._can = callback()
 
