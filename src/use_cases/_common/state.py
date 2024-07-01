fc5e41ffd20b49bb620e343c329af48d3cfb7a58
Kayo <kayo.riccelo@hotmail.com>
2024-06-30 23:15:34 -0300

    refactor: updated base state.

--- src/use_cases/_common/state.py
+++ src/use_cases/_common/state.py
@@ -1,41 +1,22 @@
 
-from src.core.exceptions.messages import NOT_FOUND_IN_MESSAGE_EXCEPTION, OPERATION_WITHOUT_PERMISSION_MESSAGE_EXCEPTION
+from src.core.exceptions.messages import (
+    CHANGE_STATUS_NOT_ALLOWED_MESSAGE_EXCEPTION, NOT_FOUND_IN_MESSAGE_EXCEPTION, OPERATION_WITHOUT_PERMISSION_MESSAGE_EXCEPTION
+)
 from src.core.exceptions.types import SystemException, UseCaseStateException
 
 
 class BaseState:
-    """
-    BaseState class is a base class for all states in the application.
+    _exception: bool = True
+    _can: bool = True
+    _kwargs: dict = {}
+    _field_name: str = 'status'
+    _status_permission: list = []
+    _instance: object = None
 
-    It provides a set of common methods and properties that can be used by all states.
+    def __init__(self, instance) -> None:
+        self._instance = instance
 
-    Attributes:
-        _exception (bool): A flag indicating whether to raise an exception if the operation is not allowed.
-        _can (bool): A flag indicating whether the operation is allowed.
-        _kwargs (dict): A dictionary of keyword arguments.
-        _field_name (str): The name of the field that stores the status.
-    """
-
-    _exception = True
-    _can = True
-    _kwargs = {}
-    _field_name = 'status'
-
-    def _get_value_in_kwargs(self, name, required=True):
-        """
-        A helper method to get a value from the keyword arguments.
-
-        Args:
-            name (str): The name of the argument.
-            required (bool, optional): A flag indicating whether the argument is required. Defaults to True.
-
-        Raises:
-            SystemException: If the argument is not found and required is True.
-
-        Returns:
-            Any: The value of the argument.
-        """
-        
+    def _get_value_in_kwargs(self, name: str, required: bool = True) -> any:
         value = self._kwargs.get(name, None)
 
         if not value and required:
@@ -43,52 +24,17 @@ class BaseState:
 
         return value
 
-    @property
-    def status(self):
-        """
-        A property that returns the status. It will retrieve the value from the instance if it hasn't been set yet.
-
-        Returns:
-            Any: The status.
-        """
-
-        if not hasattr(self, '_status'):
-            instance = self._get_value_in_kwargs('instance')
-
-            value = getattr(instance, self._field_name)
-
-            setattr(self, '_status', value)
-
-        return getattr(self, '_status')
-
-    def execute_exception(self):
-        """
-        A method that executes the exception logic. It will check the _exception flag and raise an exception if necessary.
-
-        Returns:
-            bool: The _can flag.
-        """
-
+    def execute_exception(self) -> bool:
         self._exception = self._kwargs.get('exception', self._exception)
 
         if not self._can and self._exception:
             raise UseCaseStateException(None, OPERATION_WITHOUT_PERMISSION_MESSAGE_EXCEPTION)
 
         return self._can
+    
+    def set_status(self, status):
+        if not status in self._status_permission:
+            raise UseCaseStateException(None, CHANGE_STATUS_NOT_ALLOWED_MESSAGE_EXCEPTION)
 
-    def execute_callback(self, callbacks):
-        """
-        A method that executes the callback logic. It will iterate through the callbacks and check the _can flag.
-
-        Args:
-            callbacks (list): A list of callbacks.
-
-        Returns:
-            bool: The _can flag.
-        """
-
-        for callback in callbacks:
-            if self._can:
-                self._can = callback()
-
-        return self.execute_exception()
+        setattr(self._instance, self._field_name, status)
+        self._instance.save()
