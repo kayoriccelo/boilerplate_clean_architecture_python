1ad7147cb21c197f225ad4785a8655097dcfa64a
Kayo <kayo.riccelo@hotmail.com>
2024-06-30 23:14:34 -0300

    refactor: adapted state builder in business.

--- src/use_cases/_common/business.py
+++ src/use_cases/_common/business.py
@@ -1,10 +1,4 @@
 
-from src.core.exceptions.messages import (
-    ACCESS_PAGE_LISTING_MESSAGE_EXCEPTION, CREATE_LISTING_RECORD_MESSAGE_EXCEPTION, CREATE_RECORD_MESSAGE_EXCEPTION
-)
-from src.core.exceptions.types import UseCaseBusinessException
-
-
 class BaseBusiness:
     """
     Base Business class that implements the core business logic for the application.
@@ -18,141 +12,24 @@ class BaseBusiness:
         state_class (type): The state class that is used to manage the state of the system.
     """
 
-    entity_class = None
-    rules_class = None
-    state_class = None
+    _kwargs: dict = {}
+    entity_class: any = None
+    rules_class: any = None
+    state_builder_class: any = None
 
     def __init__(self, repository: object):
-        """
-        Initialize the BaseBusiness class.
-
-        Args:
-            repository (object): The repository object that is used to interact with the data store.
-        """
-
         self.repository = repository
+
+    @property
+    def state(self):
+        if not hasattr(self, '_state'):
+            builder_state = self.state_builder_class()
+            state = builder_state.build(instance=self._kwargs['instance'])
+
+            setattr(self, '_state', state)
+
+        return getattr(self, '_state')
+
+    @property
+    def _rule(self):
         self.rule = self.rules_class()
-        self.state = self.state_class()
-
-    def get(self, pk: int) -> object:
-        """
-        Get an entity from the data store based on the specified primary key.
-
-        Args:
-            pk (int): The primary key of the entity to retrieve.
-
-        Returns:
-            The entity that was retrieved from the data store.
-
-        Raises:
-            UseCaseBusinessException: If an error occurs while retrieving the entity.
-        """
-
-        instance = self.repository.get(pk)
-
-        try:
-            instance_entity = self.entity_class(**instance.asdict())
-        
-        except Exception as err:
-            raise UseCaseBusinessException(err, CREATE_RECORD_MESSAGE_EXCEPTION)
-        
-        return instance_entity
-
-    def available(self, page: int, page_size: int) -> dict:
-        """
-        Get a list of available entities from the data store.
-
-        Args:
-            page (int): The page number of the results to retrieve.
-            page_size (int): The number of results to retrieve per page.
-
-        Returns:
-            A dictionary containing the count of available entities, the number of pages of results, and the list of entities.
-
-        Raises:
-            UseCaseBusinessException: If an error occurs while retrieving the entities.
-        """
-
-        pages = []
-        results = []
-
-        available = self.repository.available()
-
-        try:
-            available = [self.entity_class(**available.asdict()) for available in available]
-
-        except Exception as err:
-            raise UseCaseBusinessException(err, CREATE_LISTING_RECORD_MESSAGE_EXCEPTION)
-        
-        if len(available) > 0:
-            try:
-                pages = [available[i:i+page_size] for i in range(0, len(available), page_size)]
-            
-                results = pages[page - 1]
-
-            except Exception as err:
-                raise UseCaseBusinessException(err, ACCESS_PAGE_LISTING_MESSAGE_EXCEPTION % page)
-        
-        return {
-            'count': len(available),
-            'pages': len(pages),
-            'results': results
-        }
-
-    def create(self, **kwargs):
-        """
-        Create an entity in the data store.
-
-        Args:
-            **kwargs: The keyword arguments that are used to create the entity.
-
-        Returns:
-            The entity that was created in the data store.
-
-        Raises:
-            UseCaseBusinessException: If an error occurs while creating the entity.
-        """
-
-        self.rule.can_create(**kwargs)
-
-        return self.repository.create(kwargs['instance'])
-
-    def update(self, **kwargs):
-        """
-        Update an entity in the data store.
-
-        Args:
-            **kwargs: The keyword arguments that are used to update the entity.
-
-        Returns:
-            The entity that was updated in the data store.
-
-        Raises:
-            UseCaseBusinessException: If an error occurs while updating the entity.
-        """
-
-        self.state.can_update(**kwargs)
-
-        self.rule.can_update(**kwargs)
-
-        return self.repository.update(kwargs['instance'])
-
-    def delete(self, **kwargs):
-        """
-        Delete an entity from the data store.
-
-        Args:
-            **kwargs: The keyword arguments that are used to delete the entity.
-
-        Returns:
-            The entity that was deleted from the data store.
-
-        Raises:
-            UseCaseBusinessException: If an error occurs while deleting the entity.
-        """
-
-        self.state.can_delete(**kwargs)
-
-        self.rule.can_delete(**kwargs)
-
-        return self.repository.delete(kwargs['instance'])
--- src/use_cases/account/business.py
+++ src/use_cases/account/business.py
@@ -1,11 +1,75 @@
 
+from src.core.exceptions.messages import (
+    ACCESS_PAGE_LISTING_MESSAGE_EXCEPTION, CREATE_LISTING_RECORD_MESSAGE_EXCEPTION, 
+    CREATE_RECORD_MESSAGE_EXCEPTION
+)
+from src.core.exceptions.types import UseCaseBusinessException
+from src.use_cases.account.builders import AccountStateBuilder
 from src.use_cases.account.rules import AccountRules
 from src.use_cases._common.business import BaseBusiness
 from src.domain.entities.account import Account
-from src.use_cases.account.state import AccountState
 
 
 class AccountBusiness(BaseBusiness):
     entity_class = Account
     rules_class = AccountRules
-    state_class = AccountState
+    state_builder_class = AccountStateBuilder
+
+    def get(self, pk: int) -> object:
+        instance = self.repository.get(pk)
+
+        try:
+            instance_entity = self.entity_class(**instance.asdict())
+        
+        except Exception as err:
+            raise UseCaseBusinessException(err, CREATE_RECORD_MESSAGE_EXCEPTION)
+        
+        return instance_entity
+
+    def available(self, page: int, page_size: int) -> dict:
+        pages = []
+        results = []
+
+        available = self.repository.available()
+
+        try:
+            available = [self.entity_class(**available.asdict()) for available in available]
+
+        except Exception as err:
+            raise UseCaseBusinessException(err, CREATE_LISTING_RECORD_MESSAGE_EXCEPTION)
+        
+        if len(available) > 0:
+            try:
+                pages = [available[i:i+page_size] for i in range(0, len(available), page_size)]
+            
+                results = pages[page - 1]
+
+            except Exception as err:
+                raise UseCaseBusinessException(err, ACCESS_PAGE_LISTING_MESSAGE_EXCEPTION % page)
+        
+        return {
+            'count': len(available),
+            'pages': len(pages),
+            'results': results
+        }
+
+    def create(self, **kwargs):
+        self._kwargs = kwargs
+
+        self.rule.can_create(**kwargs)
+
+        return self.repository.create(kwargs['instance'])
+
+    def update(self, **kwargs):
+        self.state.can_update(**kwargs)
+
+        self.rule.can_update(**kwargs)
+
+        return self.repository.update(kwargs['instance'])
+
+    def delete(self, **kwargs):
+        self.state.can_delete(**kwargs)
+
+        self.rule.can_delete(**kwargs)
+
+        return self.repository.delete(kwargs['instance'])
